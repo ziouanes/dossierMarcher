f5fcf86295ee7129044aab60da92e8bc029c097d
hamza ziouane <zioianhamza@gmail.com>
2021-06-01 11:55:09 +0100

    test

--- resources/views/layouts/footer-scripts.blade.php
+++ resources/views/layouts/footer-scripts.blade.php
@@ -1,16 +1,17 @@
 <!-- COMMON SCRIPTS -->
-<script src="{{URL::asset('assets/plugins/jquery-2.2.4.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/common_scripts_min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/validate.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/jquery.tweet.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/functions.js"')}}"></script>
+<script src="{{ URL::asset('assets/plugins/jquery-2.2.4.min.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/common_scripts_min.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/validate.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/jquery.tweet.min.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/functions.js"') }}"></script>
+<script src="{{ URL::asset('assets/plugins/lazy_load.js"') }}"></script>
 
-	 <!-- SPECIFIC SCRIPTS -->
-     <script src="{{URL::asset('assets/layerslider/js/greensock.js')}}"></script>
-     <script src="{{URL::asset('assets/layerslider/js/layerslider.transitions.js')}}"></script>
-     <script src="{{URL::asset('assets/layerslider/js/layerslider.kreaturamedia.jquery.js')}}"></script>
-    
-   
+<!-- SPECIFIC SCRIPTS -->
+<script src="{{ URL::asset('assets/layerslider/js/greensock.js') }}"></script>
+<script src="{{ URL::asset('assets/layerslider/js/layerslider.transitions.js') }}"></script>
+<script src="{{ URL::asset('assets/layerslider/js/layerslider.kreaturamedia.jquery.js') }}"></script>
+
+
 
 
 
@@ -25,22 +26,25 @@
         responsiveUnder: 1280,
         layersContainer: 1200,
         skinsPath: 'layerslider/skins/'
-            // Please make sure that you didn't forget to add a comma to the line endings
-            // except the last line!
+        // Please make sure that you didn't forget to add a comma to the line endings
+        // except the last line!
     });
+
 </script>
 
- <script src="{{URL::asset('assets/plugins/jquery.cookiebar.js')}}"></script>
- <script src="{{URL::asset('assets/plugins/lazy_load')}}"></script>
+<script src="{{ URL::asset('assets/plugins/jquery.cookiebar.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/lazy_load') }}"></script>
 <script>
     'use strict';
     $.cookieBar({
         fixed: true
     });
+
 </script>
 <script>
-    $(document).ready(function(){
+    $(document).ready(function() {
         $("img").lazy();
     })
+
 </script>
 @yield('js')
